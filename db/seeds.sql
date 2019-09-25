f4bbb95b02ed00eaad6df80c6fb48ac79711ad7a
Bigshmow <dstewart88@gmail.com>
2019-09-25 16:45:02 -0700

    added css and bootstrap to make it pretty!

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers_tb (burger_name,devoured) VALUES ('Cheese Burger',TRUE);
+INSERT INTO burgers_tb (burger_name,devoured) VALUES ('Cheese Burger',FALSE);
 INSERT INTO burgers_tb (burger_name,devoured) VALUES ('Ham Burger', FALSE);
-INSERT INTO burgers_tb (burger_name,devoured) VALUES ('BBQ Burger',TRUE);
+INSERT INTO burgers_tb (burger_name,devoured) VALUES ('BBQ Burger',FALSE);
 INSERT INTO burgers_tb (burger_name,devoured) VALUES ('Double Cheese Burger',FALSE);
