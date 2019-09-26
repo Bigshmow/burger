09073a3e54ad03a6f362259c394de8cd8b09efbb
Bigshmow <dstewart88@gmail.com>
2019-09-25 17:03:16 -0700

    jaws code added

--- config/connection.js
+++ config/connection.js
@@ -12,6 +12,17 @@ var source = {
 
 var connection = mysql.createConnection(source.localhost);
 
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '6Nodagem7!',
+        database: 'burgers_db'
+    });
+};
+
 connection.connect(function(err){
     if (err) {
         console.log("error connecting: " + err.stack);
